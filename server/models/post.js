dd715e2d605c3a63f0e1ecfcad4ec0340461f2d1
Himanshu Bhoir <himanshubhoir25@gmail.com>
2022-10-09 14:39:16 +0530

    createpost-but-resolved

--- server/models/post.js
+++ server/models/post.js
@@ -12,7 +12,7 @@ const postSchema = new mongoose.Schema({
     },
     photo:{
         type: String,
-        default: "no photo"
+        default: ""
     },
     postedById:{
         type: ObjectId,
